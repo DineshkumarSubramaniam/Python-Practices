3a2fa0f55e613090cee09b3402ce8b664ad90312
Dineshkumar Subramaniam <113435454+DineshkumarSubramaniam@users.noreply.github.com>
2022-12-15 10:52:22 +0530

    Update Break.py

--- Break.py
+++ Break.py
@@ -3,9 +3,13 @@
 
 Number = int(input("\nEnter any number : "))
 print("\nThe output is : ")
-
-for i in range(1, Number):   # Used to check the number
-    if i % 9 == 0:           # Used to check does the number has the multiples of 9
-        break                # Used for break if the given number has multiples of 9
-    print(i, end="  ")        # Used to print
-print("\n")                  # Used to print new line
+# Used to check the number
+for i in range(1, Number): 
+# Used to check does the number has the multiples of 9
+    if i % 9 == 0:  
+# Used for break if the given number has multiples of 9
+        break    
+# Used to print
+    print(i, end="  ")   
+# Used to print new line
+print("\n")                  
